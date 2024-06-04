6352b293a3adbed14d8ef929bc199f0a593c501f
Ahmed Allawi <83989859+aallawi@users.noreply.github.com>
2024-06-05 02:57:14 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -22,24 +22,3 @@ export default function Home() {
   );
 }
 
-// import Header from "./components/Header";
-// import Hero from "./components/Hero";
-// import Cars from "./components/Cars";
-// import About from "./components/About";
-// import Testimonial from "./components/Testimonial";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// export default function Home() {
-//   return (
-//     <main className="max-w-[1920px] body mx-auto relative overflow-hidden">
-//       <Header />
-//       <Hero />
-//       <Cars />
-//       <About />
-//       <Testimonial />
-//       <Contact />
-//       <Footer />
-//     </main>
-//   );
-// }
